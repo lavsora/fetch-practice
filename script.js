434081a90954b8a643ad8f7b0e8bde2cdf62c7e8
lavsora <ya.karpich@gmail.com>
2022-07-02 15:25:42 +0300

    fetch practice

---
+++ script.js
@@ -0,0 +1,31 @@
+const ajaxData = ({ url, data, method }) => {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+}
+
+const getData = () => {
+    return ajaxData({
+        method: 'GET',
+        url: 'db.json',
+    })
+}
+
+const sendData = (data) => {
+    return ajaxData({
+        method: 'POST',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        data: data
+    })
+        .then(data => console.log(data))
+        .catch((error) => console.log(error))
+}
+
+getData()
+    .then(data => sendData(data))
+    .catch((error) => console.log(error))
